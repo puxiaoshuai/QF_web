614309d37254364e016dc34c3b1a5cfef3e0eaad
puxiaoshuai <1372553910@qq.com>
2019-12-26 22:36:33 +0800

    react-redux

---
+++ day22react/react_routerdemo/demo-rouer/src/demo1/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Demo extends Component{
+    render() {
+        return (
+            <div>
+                11
+            <h1>jfkjsadkfsadklf</h1>
+            </div>
+        )
+    }
+}
