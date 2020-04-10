f6b693908608f747e50b68e9ca9fb8c9fa901c31
puxiaoshuai <1372553910@qq.com>
2020-04-10 13:26:12 +0800

    提交

--- blog/blog_front/config/apiUrl.js
+++ blog/blog_front/config/apiUrl.js
@@ -8,7 +8,7 @@ let ipUrl="http://127.0.0.1:8000/"
 let servicePath={
     getArticleList:ipUrl+"blog" ,  //  首页文章列表接口
     //getArticleById:ipUrl + 'getArticleById/',  // 文章详细页内容接口 ,需要接收参数
-    getTypeInfo:ipUrl+"getTypeInfo",//获取文章分类
+    getTypeInfo:ipUrl+"getTypeInfo/",//获取文章分类
     getListById:ipUrl+"getListById/?id="
 }
 export default servicePath
