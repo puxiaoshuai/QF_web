ef2c5d3de4ba9ba668626a9c6921e67673f1d775
puxiaoshuai <1372553910@qq.com>
2020-08-18 23:16:46 +0800

    webpack

--- day24webpack/02webpack配置/webpack.config.js
+++ day24webpack/02webpack配置/webpack.config.js
@@ -1,21 +1,40 @@
-const path=require("path")
-module.exports={
+const path = require("path")
+module.exports = {
 	//输入路径
-	entry:"./src/index.js",
+	entry: "./src/index.js",
 	//输出路径
-	output:{
-		path:path.resolve(__dirname,"dist"),
-		filename:"bundle.js"
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+		publicPath:"dist/"
 	},
 	module: {
-	    rules: [
-	      {
-	        test: /\.css$/,
-	        use: [
-	          { loader: "style-loader" },
-	          { loader: "css-loader" }
-	        ]
-	      }
-	    ]
-	  }
+		rules: [{
+				test: /\.css$/,
+				use: [{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader"
+					}
+				]
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 8192
+					}
+				}]
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {}
+				}]
+			}
+		]
+	}
 }
