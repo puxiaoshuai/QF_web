8d96a78572f21a6f851949c14a4f0dc34287f4f4
puxiaoshuai <1372553910@qq.com>
2021-01-19 17:39:47 +0800

    koa2

---
+++ day19KOA/koa_app/app.js
@@ -0,0 +1,43 @@
+const Koa = require('koa')
+const app = new Koa()
+
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+// const mongoose = require('mongoose')
+// const dbConfig=require("./dbs/config.js")
+
+// error handler
+onerror(app)
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+// mongoose.connect(dbConfig.dbs,{
+//   useNewUrlParser:true
+// })
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
